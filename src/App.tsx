94419e699c7d6f54e149225b7a706368b09d5d75
Witek <witek.cynk06@gmail.com>
2025-05-20 14:04:24 +0200

    hangman

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
 import * as React from "react";
-import Button from "react-bootstrap/Button";
-
-import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
+//import { stat } from "fs";
 
 let allWords = [
   "apple",
@@ -60,12 +58,23 @@ let allWords = [
 type Interval = typeof setInterval;
 type IntervalRef = ReturnType<Interval>;
 
+
 function App() {
   const timerRef = React.useRef<ReturnType<typeof setInterval>>(0);
   const inputRef = React.useRef<HTMLInputElement>(null);
   const [words, setWords] = React.useState<string[]>([]);
+  const [hidden_words, setHidden_words] = React.useState('');
   const [timeElapsed, setTimeElapsed] = React.useState(0);
-
+  const [inputLetter, setInputLetter] = React.useState<string>('');
+  const [actualword, setActualword] = React.useState<string>('');
+  const [guessedLetters, setGuessedLetters] = React.useState<string[]>([]);
+  const [wrongGuesses, setWrongGuesses] = React.useState(0);
+  const [status, setStatus] = React.useState<string>('playing');
+  
+  const maxTries = 10;
+  const rndnum = Math.floor(Math.random() * allWords.length);
+  const word = allWords[rndnum];
+  const hidden_word = ('_'.repeat(word.length).trim());
   React.useEffect(() => {
     timerRef.current = setInterval(() => {
       setTimeElapsed((prev) => prev + 1);
@@ -85,10 +94,10 @@ function App() {
   return (
     <div>
       <div>{timeElapsed}</div>
-      <Button type="button" onClick={pauseTimer}>
+      <button type="button" onClick={pauseTimer}>
         Reset timer
-      </Button>
-      <Button
+      </button>
+      <button
         type="button"
         disabled={allWords.length === 0}
         onClick={() => {
@@ -96,17 +105,50 @@ function App() {
           const word = allWords[rndnum];
           setWords((prevWords) => [word, ...prevWords]);
           allWords = allWords.filter((_, index) => index !== rndnum);
-        }}
+          const hidden_word = ('_'.repeat(word.length).trim());
+        }  
+      }
       >
-        SUBMIT
-      </Button>{" "}
+        Randomize
+      </button>{" "}
       <br />
       <ul>
-        {words.map((word) => (
-          <li key={word}>{word}</li>
-        ))}
+          <li key={word}>{hidden_word}</li>
       </ul>
       <input ref={inputRef} />
+      <button
+        type="button"
+        onClick={() => {
+          React.useEffect(() => {
+            const letter = inputLetter;
+            if(guessedLetters.includes(letter) || status !== 'playing'){
+              setInputLetter(letter);        
+            }
+            else if(letter && status === 'playing'){
+              const guessed = [...guessedLetters, letter];
+              setGuessedLetters(guessed);
+            
+            if(!word.includes(letter)){
+              const wrong = wrongGuesses + 1;
+              setWrongGuesses(wrong);
+            }
+
+            if(wrong >= maxTries){
+              setStatus('lost');
+            }
+            }
+            else{
+              const allGuessed = word.split('').every((char) => guessed.includes(char));
+              if (allGuessed) {
+                setStatus('won');
+              }
+            }
+            setInputLetter('');
+            },[inputLetter]);
+        }} 
+      >
+        Check
+      </button>
     </div>
   );
 }
