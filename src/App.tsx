771911fedbbfd13f39a9463519aab37848e6e88a
Witek <witek.cynk06@gmail.com>
2025-05-27 10:21:16 +0200

    plansza

---
+++ src/App.tsx
@@ -0,0 +1,121 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return(
+      <div className="gridContainer">
+        <div className="box"></div>
+        <div className="box">A</div>
+        <div className="box">B</div>
+        <div className="box">C</div>
+        <div className="box">D</div>
+        <div className="box">E</div>
+        <div className="box">F</div>
+        <div className="box">G</div>
+        <div className="box">H</div>
+        <div className="box"></div>
+
+        <div className="box">8</div>
+        <div className="box1"></div>
+        <div className="box"></div>
+        <div className="box1"></div>
+        <div className="box"></div>
+        <div className="box1"></div>
+        <div className="box"></div>
+        <div className="box1"></div>
+        <div className="box"></div>
+        <div className="box">8</div>
+
+        <div className="box">7</div>
+        <div className="box"></div>
+        <div className="box1"></div>
+        <div className="box"></div>
+        <div className="box1"></div>
+        <div className="box"></div>
+        <div className="box1"></div>
+        <div className="box"></div>
+        <div className="box1"></div>
+        <div className="box">7</div>
+
+        <div className="box">6</div>
+        <div className="box1"></div>
+        <div className="box"></div>
+        <div className="box1"></div>
+        <div className="box"></div>
+        <div className="box1"></div>
+        <div className="box"></div>
+        <div className="box1"></div>
+        <div className="box"></div>
+        <div className="box">6</div>
+
+        <div className="box">5</div>
+        <div className="box"></div>
+        <div className="box1"></div>
+        <div className="box"></div>
+        <div className="box1"></div>
+        <div className="box"></div>
+        <div className="box1"></div>
+        <div className="box"></div>
+        <div className="box1"></div>
+        <div className="box">5</div>
+
+        <div className="box">5</div>
+        <div className="box1"></div>
+        <div className="box"></div>
+        <div className="box1"></div>
+        <div className="box"></div>
+        <div className="box1"></div>
+        <div className="box"></div>
+        <div className="box1"></div>
+        <div className="box"></div>
+        <div className="box">4</div>
+
+        <div className="box">3</div>
+        <div className="box"></div>
+        <div className="box1"></div>
+        <div className="box"></div>
+        <div className="box1"></div>
+        <div className="box"></div>
+        <div className="box1"></div>
+        <div className="box"></div>
+        <div className="box1"></div>
+        <div className="box">3</div>
+
+        <div className="box">2</div>
+        <div className="box1"></div>
+        <div className="box"></div>
+        <div className="box1"></div>
+        <div className="box"></div>
+        <div className="box1"></div>
+        <div className="box"></div>
+        <div className="box1"></div>
+        <div className="box"></div>
+        <div className="box">2</div>
+
+        <div className="box">1</div>
+        <div className="box"></div>
+        <div className="box1"></div>
+        <div className="box"></div>
+        <div className="box1"></div>
+        <div className="box"></div>
+        <div className="box1"></div>
+        <div className="box"></div>
+        <div className="box1"></div>
+        <div className="box">1</div>
+
+        <div className="box"></div>
+        <div className="box">A</div>
+        <div className="box">B</div>
+        <div className="box">C</div>
+        <div className="box">D</div>
+        <div className="box">E</div>
+        <div className="box">F</div>
+        <div className="box">G</div>
+        <div className="box">H</div>
+        <div className="box"></div>
+      </div>
+  )
+}
+
+export default App
