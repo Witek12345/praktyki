1c8f6455677720556d3563e819cf89c436058b1b
Dawid Bulinski <dawid.bulinski@xebia.com>
2025-05-29 10:05:38 +0200

    notatki po spotkaniu

--- src/App.tsx
+++ src/App.tsx
@@ -1,119 +1,41 @@
 import { useState } from 'react'
 import './App.css'
 
+type BoardItem = {
+  x: number;
+  y: number;
+  value: 'white-pawn' | 'black-pawn' | 'white-queen' | 'black-queen' | 'empty';
+}
+
+const generateBoard = (size: number): BoardItem[] => {
+  const items: BoardItem[] = [];
+  for (let y = 1; y < size + 1; y++) {
+    for (let x = 1; x < size + 1; x++) {
+      items.push({
+        x,
+        y,
+        value: 'empty'
+      });
+    }
+  }
+  return items;
+}
+
+const getColor = (x: number, y: number): 'box-white' | 'box-black' => {
+    if ((x + y) % 2 === 0) {
+        return 'box-white';
+    }
+    return 'box-black';
+}
+
 function App() {
+  const [board] = useState<BoardItem[]>(generateBoard(8));
 
   return(
       <div className="gridContainer">
-        <div className="box"></div>
-        <div className="box">A</div>
-        <div className="box">B</div>
-        <div className="box">C</div>
-        <div className="box">D</div>
-        <div className="box">E</div>
-        <div className="box">F</div>
-        <div className="box">G</div>
-        <div className="box">H</div>
-        <div className="box"></div>
-
-        <div className="box">8</div>
-        <div className="box1"></div>
-        <div className="box"></div>
-        <div className="box1"></div>
-        <div className="box"></div>
-        <div className="box1"></div>
-        <div className="box"></div>
-        <div className="box1"></div>
-        <div className="box"></div>
-        <div className="box">8</div>
-
-        <div className="box">7</div>
-        <div className="box"></div>
-        <div className="box1"></div>
-        <div className="box"></div>
-        <div className="box1"></div>
-        <div className="box"></div>
-        <div className="box1"></div>
-        <div className="box"></div>
-        <div className="box1"></div>
-        <div className="box">7</div>
-
-        <div className="box">6</div>
-        <div className="box1"></div>
-        <div className="box"></div>
-        <div className="box1"></div>
-        <div className="box"></div>
-        <div className="box1"></div>
-        <div className="box"></div>
-        <div className="box1"></div>
-        <div className="box"></div>
-        <div className="box">6</div>
-
-        <div className="box">5</div>
-        <div className="box"></div>
-        <div className="box1"></div>
-        <div className="box"></div>
-        <div className="box1"></div>
-        <div className="box"></div>
-        <div className="box1"></div>
-        <div className="box"></div>
-        <div className="box1"></div>
-        <div className="box">5</div>
-
-        <div className="box">5</div>
-        <div className="box1"></div>
-        <div className="box"></div>
-        <div className="box1"></div>
-        <div className="box"></div>
-        <div className="box1"></div>
-        <div className="box"></div>
-        <div className="box1"></div>
-        <div className="box"></div>
-        <div className="box">4</div>
-
-        <div className="box">3</div>
-        <div className="box"></div>
-        <div className="box1"></div>
-        <div className="box"></div>
-        <div className="box1"></div>
-        <div className="box"></div>
-        <div className="box1"></div>
-        <div className="box"></div>
-        <div className="box1"></div>
-        <div className="box">3</div>
-
-        <div className="box">2</div>
-        <div className="box1"></div>
-        <div className="box"></div>
-        <div className="box1"></div>
-        <div className="box"></div>
-        <div className="box1"></div>
-        <div className="box"></div>
-        <div className="box1"></div>
-        <div className="box"></div>
-        <div className="box">2</div>
-
-        <div className="box">1</div>
-        <div className="box"></div>
-        <div className="box1"></div>
-        <div className="box"></div>
-        <div className="box1"></div>
-        <div className="box"></div>
-        <div className="box1"></div>
-        <div className="box"></div>
-        <div className="box1"></div>
-        <div className="box">1</div>
-
-        <div className="box"></div>
-        <div className="box">A</div>
-        <div className="box">B</div>
-        <div className="box">C</div>
-        <div className="box">D</div>
-        <div className="box">E</div>
-        <div className="box">F</div>
-        <div className="box">G</div>
-        <div className="box">H</div>
-        <div className="box"></div>
+        {board.map((item) => (
+            <div key={`${item.x}-${item.y}`} style={{ gridColumn: item.x, gridRow: item.y }} className={`box ${getColor(item.x, item.y)}`} />)
+        )}
       </div>
   )
 }
